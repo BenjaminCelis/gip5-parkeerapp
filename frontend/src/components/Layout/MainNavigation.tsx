ee29fa553e5c13c2d38689edbd7806bfe57d8ab6
Benjamin Celis <benjamin.celis@student.ucll.be>
2022-03-17 16:18:38 +0100

    Navbar& footer

--- frontend/src/components/Layout/MainNavigation.tsx
+++ frontend/src/components/Layout/MainNavigation.tsx
@@ -5,21 +5,29 @@ import React from "react";
 
 function MainNavigation() {
     return <>
-        <Navbar bg="#153965" variant="dark">
-            <Container>
-                <img
-                    src={require('../../images/carparkingimg.png')}
-                    width="150"
-                    height="150"
-                    className ="navimg"
-                    alt="React Bootstrap logo"
-                />
-                <Link to="/"><h1>Rent-A-Spot</h1></Link>
-                <Nav className="me-auto">
-
-                </Nav>
-            </Container>
-        </Navbar>
+        <header className="header">
+            <nav>
+                <ul>
+                    <li>
+                        <img
+                            src={require('../../images/carparkingimg.png')}
+                            width="150"
+                            height="150"
+                            className ="navimg"
+                        />
+                    </li>
+                    <li>
+                        <Link to="/"><h1>Rent-A-Spot</h1></Link>
+                    </li>
+                    <li>
+                        <Link to="/reservation"><h2>Reservations</h2></Link>
+                    </li>
+                    <li>
+                        <Link to="/reservation/create"><h2>New reservation</h2></Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     </>
 
 
